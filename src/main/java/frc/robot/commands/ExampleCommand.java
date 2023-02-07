4478127f18d93d601db9f46b11fc5f48899b4064
Your Name <you@example.com>
2023-02-07 16:16:58 -0600

    example test ig

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -25,7 +25,9 @@ public class ExampleCommand extends CommandBase {
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
+    public void initialize() {
+        subsystem.exampleMethodCommand().execute(); //idk if this works
+    }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
