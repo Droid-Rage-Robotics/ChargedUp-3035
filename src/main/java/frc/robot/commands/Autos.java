c22d18af9b0143119e94495591a405902641ff3d
Your Name <you@example.com>
2023-03-02 17:38:42 -0600

    Auto Paths

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,13 +1,70 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.Drive.PathPlannerFollow;
+import frc.robot.commands.ElevatorCommands.DropCone;
+import frc.robot.commands.ElevatorCommands.IntakeCube;
+import frc.robot.commands.ElevatorCommands.MoveHigh;
+import frc.robot.commands.ElevatorCommands.MoveIntakeLow;
+import frc.robot.commands.ElevatorCommands.Outtake;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Pivot;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public final class Autos {
     /** Example static factory for an autonomous command. */
-    public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-        return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+    public static CommandBase top(Drive drive, Elevator elevator, Pivot pivot, Intake intake) {
+        return PathPlannerFollow.create(drive, "Top")
+            .setMaxVelocity(0.3)
+            .setAcceleration(0.5)
+            .addMarker("preloadDrop", new SequentialCommandGroup(
+                new MoveHigh(elevator,pivot),
+                new WaitCommand(1),
+                new DropCone(elevator, pivot, intake)
+                ))
+            .addMarker("pickUp", new SequentialCommandGroup(
+                new MoveIntakeLow(elevator, pivot),
+                new IntakeCube(pivot, intake)
+                ))
+            .addMarker("dropOne", new SequentialCommandGroup(
+                new MoveHigh(elevator,pivot),
+                new WaitCommand(1),
+                new DropCone(elevator, pivot, intake)
+                ))
+            .build();
+    }
+
+    public static CommandBase mid(Drive drive, Elevator elevator, Pivot pivot, Intake intake) {
+        return PathPlannerFollow.create(drive, "Top")
+            .setMaxVelocity(0.3)
+            .setAcceleration(0.5)
+            .addMarker("preloadDrop", new SequentialCommandGroup(
+                new MoveHigh(elevator,pivot),
+                new WaitCommand(1),
+                new DropCone(elevator, pivot, intake)
+                ))
+            .build();
+    }
+
+    public static CommandBase bottom(Drive drive, Elevator elevator, Pivot pivot, Intake intake) {
+        return PathPlannerFollow.create(drive, "Top")
+            .setMaxVelocity(0.3)
+            .setAcceleration(0.5)
+            .addMarker("preloadDrop", new SequentialCommandGroup(
+                new MoveHigh(elevator,pivot),
+                new WaitCommand(1),
+                new DropCone(elevator, pivot, intake)
+                ))
+            .addMarker("pickUp", new SequentialCommandGroup(
+                new MoveIntakeLow(elevator, pivot),
+                new IntakeCube(pivot, intake)
+                ))
+            .build();
     }
 
     private Autos() {
