02baaa9afa42dd86f0cb6a74ab11b0226c409cbd
Your Name <you@example.com>
2023-03-02 12:49:22 -0600

    Moved + Renamed Manual Commands

--- src/main/java/frc/robot/commands/Manual/ManualPivot.java
+++ src/main/java/frc/robot/commands/Manual/ManualPivot.java
@@ -1,16 +1,16 @@
-package frc.robot.commands;
+package frc.robot.commands.Manual;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Pivot;
 
-public class ManualArm extends CommandBase {
+public class ManualPivot extends CommandBase {
     public static class Constants {    }
 
     private final Pivot arm;
     private final CommandXboxController operator;
     
-    public ManualArm(CommandXboxController operator, Pivot arm) {
+    public ManualPivot(CommandXboxController operator, Pivot arm) {
         this.arm = arm;
         this.operator = operator;
 
