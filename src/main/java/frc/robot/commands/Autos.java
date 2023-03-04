61fec8e990d8b4890d44c2da692b71781c40ac9f
Your Name <you@example.com>
2023-03-03 21:59:32 -0600

    Auto Things

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,17 +1,17 @@
 package frc.robot.commands;
 
 import frc.robot.commands.Drive.PathPlannerFollow;
+import frc.robot.commands.ElevatorCommands.AutoMoveMid;
 import frc.robot.commands.ElevatorCommands.DropCone;
+import frc.robot.commands.ElevatorCommands.IntakeCone;
 import frc.robot.commands.ElevatorCommands.IntakeCube;
 import frc.robot.commands.ElevatorCommands.MoveHigh;
 import frc.robot.commands.ElevatorCommands.MoveIntakeLow;
 import frc.robot.commands.ElevatorCommands.MoveMid;
-import frc.robot.commands.ElevatorCommands.Outtake;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Pivot;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -24,7 +24,7 @@ public final class Autos {
             .setMaxVelocity(0.3)
             .setAcceleration(0.4)
             .addMarker("preloadDrop", new SequentialCommandGroup(
-                new MoveHigh(elevator,pivot),
+                new AutoMoveMid(elevator,pivot),
                 new WaitCommand(1),
                 new DropCone(elevator, pivot, intake)
                 ))
@@ -44,16 +44,18 @@ public final class Autos {
     public static CommandBase mid(Drive drive, Elevator elevator, Pivot pivot, Intake intake) {
         return Commands.sequence(
             Commands.sequence(
-                new MoveMid(elevator,pivot),
+                new AutoMoveMid(elevator,pivot),
                 new WaitCommand(3),
                 new DropCone(elevator, pivot, intake),
                 new WaitCommand(3)
                 ),
             PathPlannerFollow.create(drive, "Middle")
-                .setMaxVelocity(0.3)
-                .setAcceleration(0.4)
-                
+                .setMaxVelocity(0.8)
+                .setAcceleration(0.9)
                 .build()
+
+            // Commands.sequence(new MoveIntakeLow(elevator, pivot),
+            //     new IntakeCone(pivot, intake))
         );
     }
 
@@ -62,7 +64,7 @@ public final class Autos {
             .setMaxVelocity(0.)
             .setAcceleration(0.4)
             .addMarker("preloadDrop", new SequentialCommandGroup(
-                new MoveHigh(elevator,pivot),
+                new AutoMoveMid(elevator,pivot),
                 new WaitCommand(1),
                 new DropCone(elevator, pivot, intake)
                 ))
@@ -72,18 +74,17 @@ public final class Autos {
                 ))
             .build();
     }
-    public static CommandBase strafe(Drive drive, Elevator elevator, Pivot pivot, Intake intake) {
+    public static CommandBase strafeRight(Drive drive, Elevator elevator, Pivot pivot, Intake intake) {
         return Commands.sequence(
             Commands.sequence(
-                new MoveMid(elevator,pivot),
+                new AutoMoveMid(elevator,pivot),
                 new WaitCommand(3),
                 new DropCone(elevator, pivot, intake),
                 new WaitCommand(3)
                 ),
-            PathPlannerFollow.create(drive, "Strafe")
+            PathPlannerFollow.create(drive, "StrafeBlue")
                 .setMaxVelocity(0.6)
                 .setAcceleration(0.7)
-                
                 .build()
         );
     }
---
+++ src/main/java/frc/robot/commands/ElevatorCommands/AutoMoveMid.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ElevatorCommands;
+
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Pivot;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+public class AutoMoveMid extends SequentialCommandGroup {
+    public AutoMoveMid(Elevator elevator, Pivot pivot) {
+        addCommands(
+            elevator.moveAutoMid(),
+            pivot.moveMid()
+        );
+    }
+}
