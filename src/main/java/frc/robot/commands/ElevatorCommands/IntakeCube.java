caf4ae0324b1a38547d49d6ed8b3b81c54bddd2b
Your Name <you@example.com>
2023-03-07 17:05:50 -0600

    Intake Cube Default Command

--- src/main/java/frc/robot/commands/ElevatorCommands/IntakeCube.java
+++ src/main/java/frc/robot/commands/ElevatorCommands/IntakeCube.java
@@ -5,12 +5,15 @@ import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Pivot;
 
 public class IntakeCube extends SequentialCommandGroup {
-    //TODO: Maybe make a position where the pivot iks downwards so it doesn't roll away
+    //TODO: Maybe make a position where the pivot iks downwards so cube doesn't roll away
     public IntakeCube(Pivot pivot, Intake intake, double wait) {
         addCommands(
             intake.runOpen(),
             intake.runIntakeFor(wait),
             pivot.moveHold()
         );
+    }
+    public IntakeCube(Pivot pivot, Intake intake) {
+        new IntakeCube(pivot, intake, 6);
     }
 }
