5f0797a274caf6b32745940b297625da423c1bba
Anti-Shulk <84753002+Anti-Shulk@users.noreply.github.com>
2023-03-20 02:14:50 -0500

    large is cube writer so its obvious whether its a cone or cube

--- src/main/java/frc/robot/subsystems/TrackedElement.java
+++ src/main/java/frc/robot/subsystems/TrackedElement.java
@@ -10,18 +10,22 @@ public abstract class TrackedElement {
     }
     
     private static volatile Element element = Element.CONE;
-    private static final ShuffleboardValue<String> ElementWriter = ShuffleboardValue.create(element.name(), "Tracked Element", "Misc").build();
+    private static final ShuffleboardValue<String> elementWriter = ShuffleboardValue.create(element.name(), "Tracked Element", "Misc").build();
+    private static final ShuffleboardValue<Boolean> isCubeWriter = ShuffleboardValue.create(isCube(), "Is Cube", "Misc")
+        .withSize(4, 4)
+        .build();
     
     public static synchronized Element get() {
         return TrackedElement.element;
     }
 
-    public static boolean isCone() {
-        return element == Element.CONE;
+    private static boolean isCube() {
+        return element == Element.CUBE;
     }
 
     public static synchronized void set(Element element) {
-        ElementWriter.set(element.name());
-        TrackedElement.element = element; // discord
+        elementWriter.write(element.name());
+        isCubeWriter.write(isCube());
+        TrackedElement.element = element;
     }
 }
