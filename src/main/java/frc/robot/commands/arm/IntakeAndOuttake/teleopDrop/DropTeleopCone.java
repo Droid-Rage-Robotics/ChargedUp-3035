48201b9cf70e3cd70e657d46ac004c19e9234440
Lucky <saivarshini.chinnasani@gmail.com>
2023-04-03 10:39:26 -0500

    Drop Teleop Commands

--- src/main/java/frc/robot/commands/arm/IntakeAndOuttake/teleopDrop/DropTeleopCone.java
+++ src/main/java/frc/robot/commands/arm/IntakeAndOuttake/teleopDrop/DropTeleopCone.java
@@ -4,45 +4,51 @@
 
 package frc.robot.commands.arm.IntakeAndOuttake.teleopDrop;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystem.Intake;
 import frc.robot.subsystem.Intake.Velocity;
 import frc.robot.subsystem.arm.Arm;
-public class DropTeleopCube extends SequentialCommandGroup {
-    public DropTeleopCube(Arm arm, Intake intake) {
+import frc.robot.subsystem.arm.Arm.Position;
+public class DropTeleopCone extends SequentialCommandGroup {
+    public DropTeleopCone(Arm arm, Intake intake) {
         switch(Arm.getPosition()){
             case AUTO_MID: // Should never be needed in Teleop
-                addCommands(
-                    intake.runOnce(()->intake.close(false)),
-                    new WaitCommand(0.5),
-                    intake.runOnce(()->intake.setTargetVelocity(Velocity.SHOOT_CUBE_MID))
-                );
-                break;
-            case LOW:
-                addCommands(
-                        intake.runOnce(()->intake.close(false)),
-                        new WaitCommand(0.5),
-                        intake.runOnce(()->intake.setTargetVelocity(Velocity.SHOOT_CUBE_LOW))
-                );
-                break;
-            case MID:
-                addCommands(
-                    intake.runOnce(()->intake.close(false)),
-                    new WaitCommand(0.5),
-                    intake.runOnce(()->intake.setTargetVelocity(Velocity.SHOOT_CUBE_MID))
-                );
-                break;
-            case HIGH:
                 addCommands(
                     intake.runOnce(()->intake.close(false)),
                     new WaitCommand(0.5),
                     intake.runOnce(()->intake.setTargetVelocity(Velocity.OUTTAKE))
                 );
                 break;
+            case LOW:
+                addCommands(
+                        intake.runOnce(()->intake.close(false)),
+                        new WaitCommand(0.5),
+                        intake.runOnce(()->intake.setTargetVelocity(Velocity.SHOOT_CONE_LOW))
+                );
+                break;
+            case MID:
+                addCommands(
+                    intake.runOnce(()->intake.close(false)),
+                    new WaitCommand(0.5),
+                    intake.runOnce(()->intake.setTargetVelocity(Velocity.OUTTAKE))
+                );
+                break;
+            case HIGH:
+                addCommands(
+                    intake.runOnce(()->intake.close(false)),
+                    new WaitCommand(0.5),
+                    intake.runOnce(()->intake.setTargetVelocity(Velocity.SHOOT_CONE_HIGH))
+                );
+                break;
             default:
                 addCommands(
-                    intake.runOnce(()->intake.outtake())
+                    intake.runOnce(()->intake.close(false)),
+                    arm.lowerElevatorCommand(),
+                    Commands.waitSeconds(0.2),
+                    intake.runOnce(()->intake.open(true)),
+                    arm.setPositionCommand(Position.HOLD)
                 );
                 break;
             
