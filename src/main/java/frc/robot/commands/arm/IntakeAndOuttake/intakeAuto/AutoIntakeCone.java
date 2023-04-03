6435bb9435691dc622742593042f19aa076fb069
Lucky <saivarshini.chinnasani@gmail.com>
2023-04-03 13:07:56 -0500

    Auto Intake Commands

--- src/main/java/frc/robot/commands/arm/IntakeAndOuttake/intakeAuto/AutoIntakeCone.java
+++ src/main/java/frc/robot/commands/arm/IntakeAndOuttake/intakeAuto/AutoIntakeCone.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.arm.IntakeAndOuttake;
+package frc.robot.commands.arm.IntakeAndOuttake.intakeAuto;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -6,8 +6,8 @@ import frc.robot.subsystem.Intake;
 import frc.robot.subsystem.arm.Arm;
 import frc.robot.subsystem.arm.Arm.Position;
 
-public class IntakeCone extends SequentialCommandGroup {
-    public IntakeCone(Arm arm, Intake intake, int wait) {
+public class AutoIntakeCone extends SequentialCommandGroup {
+    public AutoIntakeCone(Arm arm, Intake intake, int wait) {
         addCommands(
             intake.runOnce(()->intake.close(true)),
             intake.runIntakeFor(wait),
