dbfc67ecea68545f4b1fb19ed793491b3287b117
Lucky <saivarshini.chinnasani@gmail.com>
2023-04-11 14:17:22 -0500

    Moved ChargePartsAuto

--- src/main/java/frc/robot/commands/autoPaths/OldAutos.java
+++ src/main/java/frc/robot/commands/autoPaths/OldAutos.java
@@ -1,15 +1,19 @@
 package frc.robot.commands.autoPaths;
 
+import frc.robot.commands.drive.AutoBalance;
+import frc.robot.commands.drive.LockWheels;
 import frc.robot.commands.drive.PathPlannerFollow;
 import frc.robot.commands.intakeAndOuttake.autoDrop.DropAutoCone;
 import frc.robot.commands.intakeAndOuttake.autoIntake.AutoIntakeCube;
 import frc.robot.subsystem.Intake;
+import frc.robot.subsystem.Intake.Velocity;
 import frc.robot.subsystem.arm.Arm;
 import frc.robot.subsystem.arm.Arm.Position;
 import frc.robot.subsystem.drive.Drive;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public final class OldAutos {
 
@@ -128,6 +132,49 @@ public final class OldAutos {
     //             )
     //             .build(),
     //             drive.setOffsetCommand(drive.getRotation2d().rotateBy(Rotation2d.fromDegrees(0)).getDegrees())
+    //     );
+    // }
+
+    //Didn't Work - State Practice Match 
+    // public static CommandBase chargePlusPickUpPartsHigh(Drive drive, Arm arm, Intake intake) {
+    //     return Commands.sequence(
+    //         arm.setPositionCommand(Position.HIGH),
+    //         new WaitCommand(1.05),//HAS TO BE 1
+    //         new DropAutoCone(arm, intake),
+    //         PathPlannerFollow.create(drive, "Charge + Pickup One")
+    //             .setMaxVelocity(1.8)
+    //             .setAcceleration(1.8)
+    //             .addMarker("intake", new SequentialCommandGroup(
+    //                 new WaitCommand(1),
+    //                 intake.runOnce(intake::stop),
+    //                 arm.setPositionCommand(Position.HOLD)
+    //             )
+    //         )
+    //         .build(),
+            
+    //         //Try to use gyro to figure out when the bot is level to continue the path, or just go backwards to balance
+            
+    //         intake.runOnce(()->intake.open(true)),
+    //         arm.setPositionCommand(Position.AUTO_INTAKE_LOW),
+    //         intake.runOnce(()-> intake.setTargetVelocity(Velocity.INTAKE)),
+
+    //         PathPlannerFollow.create(drive, "Charge + Pickup Two")
+    //             .setMaxVelocity(1.8)
+    //             .setAcceleration(1.8)
+    //             .addMarker("intake", new SequentialCommandGroup(
+    //                 new WaitCommand(1),
+    //                 intake.runOnce(intake::stop),
+    //                 arm.setPositionCommand(Position.HOLD)
+    //             )
+    //         )
+    //         .build(),
+
+    //         new AutoBalance(drive),
+    //         new LockWheels(drive),
+    //         drive.driveAutoReset(),
+
+    //         arm.setPositionCommand(Position.MID),
+    //         intake.runOnce(()->intake.setTargetVelocity(Velocity.SHOOT_CUBE_MID))
     //     );
     // }
 
