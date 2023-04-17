b9caf02e0e76b353a8ddaba887011cf2eaab2270
Your Name <you@example.com>
2023-04-17 14:11:30 -0500

    Added Lights to Indicate End Game (To Test)

--- src/main/java/frc/robot/commands/LightCommand.java
+++ src/main/java/frc/robot/commands/LightCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -13,6 +14,7 @@ public class LightCommand extends CommandBase {
         INTAKE,
         ElEMENT_IN,
         TRACK_ELEMENT,
+        END_GAME,
     }
     IntakeState intakeState = IntakeState.TRACK_ELEMENT;
     Timer intakeTimer = new Timer(); 
@@ -40,7 +42,10 @@ public class LightCommand extends CommandBase {
 
     @Override
     public void execute() {//TODO:Need to fix eventually
-        if ((intake.isElementIn() && driver.getRightTriggerAxis()>0.5 && intakeTimer.get()>intakeTime)){//||elementInTimer.get()<1){
+        if(getMatchTime()<30&&getMatchTime()>28){//TODO:Test
+            intakeState=IntakeState.END_GAME;
+        }
+        else if ((intake.isElementIn() && driver.getRightTriggerAxis()>0.5 && intakeTimer.get()>intakeTime)){//||elementInTimer.get()<1){
             intakeState=IntakeState.ElEMENT_IN;
         } else if(driver.getRightTriggerAxis()>0.5){
             intakeState=IntakeState.INTAKE;
@@ -63,6 +68,9 @@ public class LightCommand extends CommandBase {
                 driver.getHID().setRumble(RumbleType.kBothRumble, 0);
                 light.trackElementLight();
                 break;
+            case END_GAME://TODO:Test
+                light.setAlternatingColor(light.green, light.red);
+                break;
         }
         
         intake.setIntakeState(intakeState);
@@ -76,5 +84,9 @@ public class LightCommand extends CommandBase {
     @Override
     public boolean isFinished() {
         return true;
+    }
+
+    public double getMatchTime(){//TODO:test
+          return DriverStation.getMatchTime();
     }
 }
